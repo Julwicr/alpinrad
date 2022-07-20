acb9eec7cb13b484d2de8bb72cdc2b335a7355a4
Julien Rolland <rollandjulien13013@gmail.com>
2022-07-20 16:28:24 +0200

    add marker when clicked on map

--- app/javascript/controllers/new_station_controller.js
+++ app/javascript/controllers/new_station_controller.js
@@ -17,16 +17,37 @@ export default class extends Controller {
 
     let lat;
     let lon;
+    let marker = undefined;
     const formLat = document.getElementById('station_latitude');
     const formLon = document.getElementById('station_longitude');
 
-    map.on('click', function(e){
+    // Marker config
+    const alpinRadIcon = L.icon({
+      iconUrl: 'https://res.cloudinary.com/julwicr/image/upload/v1655480037/production/pin_c6jotw.svg',
+      shadowUrl: 'https://res.cloudinary.com/julwicr/image/upload/v1655533244/production/pin_shadow_hdaqsz.svg',
+
+      iconSize: [38, 95], // size of the icon
+      shadowSize:   [64, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [35, 78],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    })
+
+    map.on('click', (e) => {
+      if (marker != undefined) {
+        map.removeLayer(marker);
+      };
+
       const coord = e.latlng;
       lat = coord.lat;
       lon = coord.lng;
-      console.log(lat, lon);
+
       formLat.value = lat;
       formLon.value = lon;
+
+      marker = L.marker([lat, lon], {icon: alpinRadIcon}).addTo(map);
+      // marker.bindPopup().openPopup();
+      console.log(lat, lon);
     });
 
 
