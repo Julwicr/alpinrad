808200506fe4e27fe34ff57338988a30453d6ca9
Julien Rolland <rollandjulien13013@gmail.com>
2022-07-20 13:59:18 +0200

    add simple form gem

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,12 +3,20 @@ class StationsController < ApplicationController
     redirect_to root_path if current_user.role == 'user'
 
     @stations = Station.all
+    @station = Station.new
   end
 
   def new
   end
 
   def create
+
+    @station = Station.new(station_params)
+    if @station.save
+      redirect_to stations_path
+    else
+      render 'stations', status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +26,11 @@ class StationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def station_params
+    params.require(:station).permit(:name, :category, :description, :latitude, :longitude, :bike_capacity)
   end
 end
